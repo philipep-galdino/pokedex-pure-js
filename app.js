e637db64f20843e1dd9d5bd8be068c804aec917d
philipep-galdino <philipep.galdino@gmail.com>
2020-12-13 12:30:21 -0300

    handles promises and reduces into data for pokemon card

--- app.js
+++ app.js
@@ -12,7 +12,27 @@ const fetchPokemon = () => {
         pokemonPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
     }
 
-    
+    Promise.all(pokemonPromises)
+        .then(pokemons => {
+            /*
+                Reduces the pokemons array with each pokemon's data into a string
+                and destructures all the data - also mapped by the map function, to
+                be used and displayed into our card list.
+            */
+            const listPokemons = pokemons.reduce((accumulator, pokemon) => {
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+                
+                accumulator += `
+                    <li class="card">
+                    <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
+                        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                        <p class="card-subtitle">${types.join(' - ')}</p>
+                    </li>
+                `
+                
+                return accumulator    
+            }, '')
+        })
 }
 
 fetchPokemon()
